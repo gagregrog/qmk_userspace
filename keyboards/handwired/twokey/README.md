f240038811d6e7262a82bfcbd9d48b1f7c292c4a
Gary Gregory Rogers <gagregrog@gmail.com>
2025-03-15 19:09:39 -0700

    feat: alexa board

---
+++ keyboards/handwired/twokey/README.md
@@ -0,0 +1,4 @@
+## Wiring
+
+A3 and A2 to one switch
+A1 and A0 to the other switch
---
+++ keyboards/handwired/twokey/keymaps/alexa/rules.mk
@@ -0,0 +1,4 @@
+USER_NAME := gagregrog
+INCLUDE_SECRETS = yes
+SRC += tap_dances/tap_dances.c
+UNICODE_COMMON = yes
